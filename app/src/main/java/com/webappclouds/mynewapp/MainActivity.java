ad368e18e8ae3b6809f98e45906605b9e2353f2f
Dan Boehmke <Daniel@webappclouds.com>
2019-10-07 15:13:47 -0400

    Added Gesture Control

--- app/src/main/java/com/webappclouds/mynewapp/MainActivity.java
+++ app/src/main/java/com/webappclouds/mynewapp/MainActivity.java
@@ -1,37 +1,75 @@
 package com.webappclouds.mynewapp;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+
+    private TextView myMessage;
+    private GestureDetector gestureDetector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myMessage = findViewById(R.id.myMessage);
+        this.gestureDetector = new GestureDetector(this,this);
+        gestureDetector.setOnDoubleTapListener(this);
 
-        Button myButton = findViewById(R.id.myButton);
+    }
 
-        myButton.setOnClickListener(
-            new Button.OnClickListener(){
-                public void onClick(View v){
-                    TextView myText = findViewById(R.id.myText);
-                    myText.setText("Button Clicked");
-                }
-            }
-        );
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        myMessage.setText("SingleTapConfirmed");
+        return false;
+    }
 
-        myButton.setOnLongClickListener(
-            new Button.OnLongClickListener(){
-                public boolean onLongClick(View v){
-                    TextView myText = findViewById(R.id.myText);
-                    myText.setText("Long button press");
-                    return true;
-                }
-            }
-        );
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        myMessage.setText("SingleTapConfirmed");
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        myMessage.setText("SingleTapConfirmed");
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        myMessage.setText("SingleTapConfirmed");
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+        myMessage.setText("SingleTapConfirmed");
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        myMessage.setText("SingleTapConfirmed");
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        myMessage.setText("SingleTapConfirmed");
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        myMessage.setText("SingleTapConfirmed");
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        myMessage.setText("SingleTapConfirmed");
+        return false;
     }
 }
