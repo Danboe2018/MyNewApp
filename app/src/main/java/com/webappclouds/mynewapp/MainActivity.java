300c75f57116fb77a434221abda53858d6e2ae76
Dan Boehmke <Daniel@webappclouds.com>
2019-10-07 14:54:25 -0400

    Added button

--- app/src/main/java/com/webappclouds/mynewapp/MainActivity.java
+++ app/src/main/java/com/webappclouds/mynewapp/MainActivity.java
@@ -2,14 +2,26 @@ package com.webappclouds.mynewapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
+        setContentView(R.layout.activity_main);
 
+        Button myButton = findViewById(R.id.myButton);
 
+        myButton.setOnClickListener(
+            new Button.OnClickListener(){
+                public void onClick(View v){
+                    TextView myText = findViewById(R.id.myText);
+                    myText.setText("Button Clicked");
+                }
+            }
+        );
     }
 }
